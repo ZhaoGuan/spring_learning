ee4f7938d359076830b51c2fbed9e3eaa1a6f640
ZhaoGuan <guanzhaogz@gmail.com>
2020-08-03 10:56:21 +0800

    save

---
+++ gs-messaging-rabbitmq/src/test/java/com/gz/gsmessagingrabbitmq/GsMessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gz.gsmessagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsMessagingRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ validating-form-input/src/main/java/com/gz/validatingforminput/PersonForm.java
@@ -0,0 +1,27 @@
+package com.gz.validatingforminput;
+
+
+import lombok.Data;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class PersonForm {
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+
+
+    public String toString() {
+        return "Person(Name: " + this.name + ", Age: " + this.age + ")";
+    }
+}
+
