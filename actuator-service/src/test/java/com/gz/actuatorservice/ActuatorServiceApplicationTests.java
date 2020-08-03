dda4d1a0a20aa41de0d24ff2e1f52ebddca00074
ZhaoGuan <guanzhaogz@gmail.com>
2020-08-03 11:05:29 +0800

    save

---
+++ actuator-service/src/test/java/com/gz/actuatorservice/ActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gz.actuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
