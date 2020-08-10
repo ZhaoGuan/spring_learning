de2615d0c3cd5c795336dc3ad50a6e2acb432b6f
ZhaoGuan <guanzhaogz@gmail.com>
2020-08-10 14:14:16 +0800

    save

---
+++ securing-web/src/main/java/com/gz/sercuringweb/SercuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.gz.sercuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+
+}
---
+++ securing-web/src/test/java/com/gz/sercuringweb/SercuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gz.sercuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SercuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
