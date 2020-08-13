60edfb15dfabc34ef12433b723c0d65e6eb793cf
ZhaoGuan <guanzhaogz@gmail.com>
2020-08-13 11:37:44 +0800

    save

---
+++ accessing-data-gemfire/src/test/java/com/gz/accessingdatagemfire/AccessingDataGemfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gz.accessingdatagemfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataGemfireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
